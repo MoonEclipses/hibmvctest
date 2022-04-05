bf8608e8ffc79915719229e33d91790b19d4cd92
MoonEclipses <nagorny.omsk@mail.ru>
2022-04-05 23:16:20 +0600

    Added first controller and changed Hibernate version to 5

---
+++ src/main/java/com/suninvirgo/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package com.suninvirgo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+    @GetMapping("/list")
+    public String customerList(Model model){
+        return "list-customers";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/list-customers.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Администратор
+  Date: 05.04.2022
+  Time: 23:09
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Customer list</title>
+</head>
+<body>
+<h1>list of customers coming soon...</h1>
+</body>
+</html>
